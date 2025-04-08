59d84dcf2760bbeccfb7be0c4e262dd12d55c24e
Roxulu67 <rodrigogt03@gmail.com>
2025-04-08 08:30:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
+# Detector de Plagio para Trabajos Estudiantiles
 
+## Descripción
+    Este proyecto es un sistema de detección de plagio que identifica similitudes entre trabajos estudiantiles utilizando técnicas de procesamiento de texto y algoritmos de comparación.
+
+
+## Funcionalidades
+    Generación de documentos de texto aleatorios.
+    Preprocesamiento de documentos para análisis.
+    Cálculo de similitudes entre documentos utilizando diferentes métodos.
+    Visualización de resultados a través de gráficos.
+    Interfaz gráfica para facilitar la interacción del usuario.
+
+## Instalación
+    - Requisitos Previos
+    1. Asegúrate de tener instalado Python (versión 3.6 o superior) en tu sistema. También necesitarás las siguientes bibliotecas:
+
+        pandas
+        matplotlib
+        tkinter (generalmente incluido con Python)
+
+1. Clona el repositorio:
+   ```bash
+   git clone <URL_DEL_REPOSITORIO>
+   cd <NOMBRE_DEL_REPOSITORIO>
+
+2. Crea y activa un entorno virtual
+    python -m venv venv
+    # En Windows
+    venv\Scripts\activate
+    # En macOS/Linux
+    source venv/bin/activate
+
+3. Instala las dependencias:
+    pip install -r requirements.txt
+
+3. Ejecutar
+    1. Genera documentos en la carpeta documentos/.
+    2. Ejecuta el script principal:
+        python main.py
+    3. Los resultados se guardarán en la carpeta resultados/.
+
+
+## Integrantes del equipo
+- Darla Solis
+- Yasser Suárez
+- Rodrigo Tovar
